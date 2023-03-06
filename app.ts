a9cd07d48d322d16e9458119bb43f072f446afd1
Dhohir Pradana <dhohirkudus@gmail.com>
2023-03-07 06:57:17 +0700

    feat: skip own feed comment

--- app.ts
+++ app.ts
@@ -319,6 +319,8 @@ async function generateImage(text: string) {
     items.forEach(async (item) => {
       if (likeNcommentTimes === 0) return console.log("✅ Like Timeline Feeds 5 times finished");
 
+      if (item.user.pk === ig.state.cookieUserId) return console.log("🔄 Skip own timeline feed");
+
       // like a timeline feed
       await ig.media.like({
         mediaId: item.id,
