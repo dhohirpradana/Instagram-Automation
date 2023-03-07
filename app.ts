1a80e87259d0b6e3a8980c7e35491fc7733b9683
Dhohir Pradana <dhohirkudus@gmail.com>
2023-03-07 07:10:08 +0700

    feat: added improvements

--- app.ts
+++ app.ts
@@ -319,7 +319,7 @@ async function generateImage(text: string) {
     items.forEach(async (item) => {
       if (likeNcommentTimes === 0) return console.log("✅ Like Timeline Feeds 5 times finished");
 
-      if (item.user.username === "dhohirpradana") return console.log("🔄 Skip my own timeline feed");
+      if (item.user.pk == ig.state.cookieUserId) return console.log("❌ Skip own timeline feed");
 
       // like a timeline feed
       await ig.media.like({
